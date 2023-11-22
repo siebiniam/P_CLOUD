9179d6a0bfeecca17bf0290ff8fc97f42bb4c394
siebiniam <130034183+siebiniam@users.noreply.github.com>
2023-11-22 15:46:43 +0100

    Add files via upload

---
+++ List-ressource-siem.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.NOTES
+*****************************************************************************
+ETML
+Nom du script: List-ressource-siem.ps1
+Auteur: Tecle Siem Biniam
+Date: 15.11.23
+*****************************************************************************
+.SYNOPSIS
+Créer un script Powershell pour supprimer les ressources d’un resource-group
+
+
+.DESCRIPTION
+Ce script PowerShell permet de supprimer des ressource d'un resource-groupe 
+avec le nom du celui-ci.
+
+#>
+
+# Connexion à votre compte Azure (si ce n'est pas déjà fait)
+az login
+
+# Récupération de la liste de toutes les ressources dans l'abonnement
+$allResources = az resource list --output json | ConvertFrom-Json
+
+# Affichage des informations sur chaque ressource
+foreach ($resource in $allResources) {
+    Write-Host "Nom de la ressource: $($resource.name)"
+    Write-Host "Type de ressource: $($resource.type)"
+    Write-Host "Groupe de ressources: $($resource.resourceGroup)"
+    Write-Host "--------------------------"
+}
+
---
+++ Supp-ressource-siem.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.NOTES
+*****************************************************************************
+ETML
+Nom du script: Supp-ressource-siem.ps1
+Auteur: Tecle Siem Biniam
+Date: 15.11.23
+*****************************************************************************
+.SYNOPSIS
+Créer un script Powershell pour supprimer les ressources d’un resource-group
+
+
+.DESCRIPTION
+Ce script PowerShell permet de supprimer des ressource d'un resource-groupe 
+avec le nom du celui-ci.
+
+#>
+
+# *****************************************************************************
+
+# Remplacez ces valeurs par les vôtres
+$resourceGroupName = "NomDuResourceGroup"
+$location = "Switzerland North"
+
+# Connexion à votre compte Azure
+az login
+
+# Récupération de la liste des ressources dans le groupe de ressources
+$resources = Get-AzResource -ResourceGroupName $resourceGroupName
+
+# Suppression de chaque ressource
+foreach ($resource in $resources) {
+    $resourceName = $resource.ResourceName
+    $resourceType = $resource.ResourceType
+
+    Write-Host "Suppression de la ressource : $resourceName de type $resourceType"
+    
+    Remove-AzResourceGroup -Name $resourceGroupName -Force
+    
+}
+
+Write-Host "le groupe $resourceGroupName est supprimée"
